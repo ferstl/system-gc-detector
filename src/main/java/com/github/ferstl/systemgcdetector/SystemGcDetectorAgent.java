3415fe09c2db79df1c19cfc6dfafb77282958c06
Stefan Ferstl <st.ferstl@gmail.com>
2015-03-07 15:45:47 +0100

    Go

---
+++ src/main/java/com/github/ferstl/systemgcdetector/SystemGcDetectorAgent.java
@@ -0,0 +1,11 @@
+package com.github.ferstl.systemgcdetector;
+
+import java.lang.instrument.Instrumentation;
+
+
+public class SystemGcDetectorAgent {
+
+  public static void premain(String agentArgs, Instrumentation inst) {
+
+  }
+}
