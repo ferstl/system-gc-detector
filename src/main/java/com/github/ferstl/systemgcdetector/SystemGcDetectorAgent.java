ee42c056c54e7fc493d69b5adb6be8f5b9a084f3
Stefan Ferstl <st.ferstl@gmail.com>
2015-03-07 16:31:51 +0100

    proof of concept

--- src/main/java/com/github/ferstl/systemgcdetector/SystemGcDetectorAgent.java
+++ src/main/java/com/github/ferstl/systemgcdetector/SystemGcDetectorAgent.java
@@ -1,11 +1,52 @@
 package com.github.ferstl.systemgcdetector;
 
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
+import java.security.ProtectionDomain;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
 
 
 public class SystemGcDetectorAgent {
 
   public static void premain(String agentArgs, Instrumentation inst) {
+    inst.addTransformer(new ClassFileTransformer() {
+
+      @Override
+      public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer)
+      throws IllegalClassFormatException {
+        System.err.println(className);
+
+        if("java/lang/System".equals(className)) {
+          ClassPool classPool = ClassPool.getDefault();
+          try {
+            CtClass systemClass = classPool.get("java.lang.System");
+            CtMethod gc = systemClass.getDeclaredMethod("gc");
+            gc.insertBefore("System.out.println(\"System.gc() called\\n\"); new Exception().printStackTrace();");
+            byte[] bytecode = systemClass.toBytecode();
+            systemClass.detach();
+            return bytecode;
+          } catch (NotFoundException | CannotCompileException | IOException e) {
+            throw new RuntimeException(e);
+          }
+        }
+
+        return classfileBuffer;
+      }
+    }, true);
+
+    try {
+      inst.retransformClasses(System.class);
+    } catch (UnmodifiableClassException e) {
+      throw new RuntimeException(e);
+    }
 
   }
 }
